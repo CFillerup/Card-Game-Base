5dc9ab178939d694538e3b2152cb581c97d0675c
caeden <caedenfillerup.1@gmail.com>
2025-02-05 15:04:39 -0700

    changed card front appearance

--- MudBlazer2.Client/Models/CardBase.cs
+++ MudBlazer2.Client/Models/CardBase.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public string CardBack { get; set; }
         public string Description { get; set; }
+        public string CardType { get; set; }
         //public bool FaceUp { get; set; } 
     }
 }
