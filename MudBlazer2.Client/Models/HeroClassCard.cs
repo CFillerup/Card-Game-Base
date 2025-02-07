d102c250bfc9bbee5f1a72515594b887b0c7a7fb
CFillerup <Caedenfillerup.1@gmail.com>
2025-02-07 15:13:31 -0700

    Finialized ish Warrior, added some Scout.

--- MudBlazer2.Client/Models/HeroClassCard.cs
+++ MudBlazer2.Client/Models/HeroClassCard.cs
@@ -3,6 +3,8 @@
     public class HeroClassCard : CardBase
     {
         public Dictionary<string, string> DiceModifications { get; set; }
+        public string PrimaryDice { get; set; }
+        public string SecondaryDice { get; set; }
         public List<Ability> ClassAbilities { get; set; }
         public List<ClassAbilityCard> AbilityDeck { get; set; }
     }
